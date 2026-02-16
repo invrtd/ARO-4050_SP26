27d7fa10d6475e7489d3a2bab2be48e03a3ee83f
invrtd <68871516+invrtd@users.noreply.github.com>
2026-02-15 22:08:15 -0800

    Int/var corection (#7)
    * fixing cariables in fc1 file, and 6dof file
    
    * changes in fc1 file
    
    * changes variables infc1 to more readable and change the var in calc_dimderiv to the same vars
    ---------
    
    Co-authored-by: Ky Bui <Ky Bui@DESKTOP-U1MFNR5>
    Co-authored-by: K4lBui <buithominhky0@gmail.com>

--- init/Calc_DimDerivs.m
+++ init/Calc_DimDerivs.m
@@ -1,28 +1,28 @@
 %Longtitudinal Derivative
  %Axial
- X_u         = -(qbar * S * (2*CD1 + CDu)) / (m * U1)
- X_alpha     =  (qbar * S * (CL1 - CDalpha)) / m
+ X_u         = -(qbar * Sref * (2*CD1 + CD_u)) / (m * tas)
+ X_alpha     =  (qbar * Sref * (CL1 - CDalpha)) / m
 
  %Normal
- Z_u         = -(qbar * S * (2*CL1 + CLu)) / (m * U1)
- Z_alpha     = -(qbar * S * (CLalpha + CD1)) / m
+ Z_u         = -(qbar * Sref * (2*CL1 + CLu)) / (m * tas)
+ Z_alpha     = -(qbar * Sref * (CLalpha + CD1)) / m
 
  %Pitching
- M_u         =  (qbar * S * cbar * CMu) / (Iyy * u1) 
- M_alpha     =  (qbar * S * cbar * CMalpha) / Iyy
- M_q         =  (qbar * S * (cbar^2) * CMq) / (2 * Iyy * U1)
- M_alpha_dot =  (qbar * S * (cbar^2) * CMalpha) / (2 * Iyy * (U1))
+ M_u         =  (qbar * Sref * cbar * CMu) / (Iyy * tas) 
+ M_alpha     =  (qbar * Sref * cbar * CMalpha) / Iyy
+ M_q         =  (qbar * Sref * (cbar^2) * CMq) / (2 * Iyy * tas)
+ M_alpha_dot =  (qbar * Sref * (cbar^2) * CMalpha) / (2 * Iyy * (tas))
 
 %Latitudinal Derivative
  %Side
- Y_beta = (qbar * S * CYbeta) / m
- Y_p    = (qbar * S * b * CYp) / (2 * m * U1)
- Y_r    = (qbar * S * b * Cyr) / (2 * m * u1)
+ Y_beta = (qbar * Sref * CYbeta) / m
+ Y_p    = (qbar * Sref * Span * CYp) / (2 * m * tas)
+ Y_r    = (qbar * Sref * Span * Cyr) / (2 * m * tas)
  %Roll
- L_beta = (qbar * S * b * CLbeta) / Ixx
- L_p    = (qbar * S * (b^2) * CLp) / (2 * Ixx * U1)
- L_r    = (qbar * S * (b^2) * CLr) / (2 * Ixx * U1)
+ L_beta = (qbar * Sref * Span * CLbeta) / Ixx
+ L_p    = (qbar * Sref * (Span^2) * CLp) / (2 * Ixx * tas)
+ L_r    = (qbar * Sref * (Span^2) * CLr) / (2 * Ixx * tas)
  %Yaw
- N_beta = (qbar * S * b * CNbeta) / Izz
- N_p    = (qbar * S * (b^2) * CNp) / (2 * Izz * U1)
- N_r    = (qbar * S * (b^2) * CNr) / (2 * Izz * U1)
+ N_beta = (qbar * Sref * Span * CNbeta) / Izz
+ N_p    = (qbar * Sref * (Span^2) * CNp) / (2 * Izz * tas)
+ N_r    = (qbar * Sref * (Span^2) * CNr) / (2 * Izz * tas)
