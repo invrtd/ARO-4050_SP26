61dc9f0116439ec438636debdfd1917ef5974531
Ky Bui <Ky Bui@DESKTOP-U1MFNR5>
2026-02-15 21:01:35 -0800

    fixing cariables in fc1 file, and 6dof file

--- init/B747NonDimFC1.m
+++ init/B747NonDimFC1.m
@@ -3,40 +3,40 @@
 %% FLight Condition Data
 alt = 0;        % Altitude (ft)
 mach = 0.198;   % Mach Number
-tas = 221;      % True Air Speed (ft/s)
+U1 = 221;      % True Air Speed (ft/s)
 qbar = 58;      % Dynamic Pressure (lbs/ft^2)
 cbar = 0.25;    % CG Location
 alpha = 8.5;    % Angle of Attack (deg)
 
 %% Mass Data
-w = 564000;         % Weight (lbs)
+m = 564000;         % Weight (lbs)
 ixxb = 13700000;    % Mass Moment of Inertia XX (slug*ft^2)
 iyyb = 30500000;    % Mass Moment of Inertia YY (slug*ft^2)
 izzb = 43100000;    % Mass Moment of Inertia ZZ (slug*ft^2)
 ixzb = 830000;      % Mass Moment of Inertia XZ (slug*ft^2)
 
 %% Steady State Coefficients
-cl1 = 1.76;     % Initial Coefficient of Lift
-cd1 = 0.2630;   % Initial Coefficient of Drag
+CL1 = 1.76;     % Initial Coefficient of Lift
+CD1 = 0.2630;   % Initial Coefficient of Drag
 ctx1 = 0.2630;  % Initial Coefficient of Thrust/Force
 cm1 = 0;        % Initial Pitching Moment Coefficient
 cmt1 = 0;       % Initial Pitching Moment Coefficient from Thrust
 
 %% Longitudinal Coefficients and Stability Derivatives (Stability Axes, Dimensionless)
 cd0 = 0.0751;   % Zero-lift Drag Coefficient
-cdu = 0;        % Partial CD with respect to velocity u (x-dir)
-cdalph = 1.13;  % Partial CD with respect to angle of attack
+CDu = 0;        % Partial CD with respect to velocity u (x-dir)
+CDalpha = 1.13;  % Partial CD with respect to angle of attack
 ctxu = -0.5523; % Partial CTx with respect to velocity u
 cl0 = 0.92;     % Lift Coefficient at 0 AoA
-clu = -0.22;    % Partial CL with respect to velocity u
+CLu = -0.22;    % Partial CL with respect to velocity u
 clalph = 5.67;  % Partial CL with respect to angle of attack
 clalphdot = 6.7;% Partial CL with respect to time rate of change of AoA
-clq = 5.65;     % Partial CL with respect to pitch rate
+CLq = 5.65;     % Partial CL with respect to pitch rate
 cm0 = 0;        % Pitching Moment Coefficient at 0 AoA
-cmu = 0.071;    % Partial Cm with respect to velocity u
-cmalph = -1.45; % Partial Cm with respect to angle of attack
+CMu = 0.071;    % Partial Cm with respect to velocity u
+CMalph = -1.45; % Partial Cm with respect to angle of attack
 cmalphdot = -3.3; % Partial Cm with respect to time rate of change of AoA
-cmq = -21.4;    % Partial Cm with respect to pitch rate
+CMq = -21.4;    % Partial Cm with respect to pitch rate
 cmtu = 0;       % Partial Cm from thrust with respect to velocity u
 cmtalph = 0;    % Partial Cm from thrust with respect to AoA
 
@@ -51,16 +51,16 @@ cmih = -3;      % Induced Cm by horizontal stabilizer
 %chdele = ???;   % Partial Ch with respect to elevator deflection
 
 %% Lateral_Directional Stability Derivatives (Stability Axes, Dimensionless)
-clbeta = -0.281;    % Partial Cl with respect to sideslip angle
-clp = -0.502;       % Partial Cl with respect to roll rate
-clr = 0.195;        % Partial Cl with respect to yaw rate
-cybeta = -1.08;     % Partial CY with respect to sideslip angle
-cyp = 0;            % Partial Cl with respect to roll rate
+CLbeta = -0.281;    % Partial Cl with respect to sideslip angle
+CLp = -0.502;       % Partial Cl with respect to roll rate
+CLr = 0.195;        % Partial Cl with respect to yaw rate
+CYbeta = -1.08;     % Partial CY with respect to sideslip angle
+CYp = 0;            % Partial Cl with respect to roll rate
 cyr = 0;            % Partial Cl with respect to yaw rate
-cnbeta = 0.184;     % Partial Cn with respect to sideslip angle
+CNbeta = 0.184;     % Partial Cn with respect to sideslip angle
 cntbeta = 0;        % Partial Cn from thrust with respect to sideslip angle
-cnp = -0.222;       % Partial Cn with respect to roll rate
-cnr = -0.36;        % Partial Cn with respect to yaw rate
+CNp = -0.222;       % Partial Cn with respect to roll rate
+CNr = -0.36;        % Partial Cn with respect to yaw rate
 
 %% Lateral-Directional Control and Hinge Moment Derivatives (Stability Axes, Dimensionless)
 cldela = 0.053; % Partial Cl with respect to aileron deflection
