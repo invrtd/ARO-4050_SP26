9d51a60717971bd20b556d3513d80aac6ec9972e
invrtd <68871516+invrtd@users.noreply.github.com>
2026-03-03 12:05:29 -0800

    Compute env no longer has const, everything defined with vars (#20)

--- init/Sim6DOF_Init.m
+++ init/Sim6DOF_Init.m
@@ -19,6 +19,16 @@ mu0 = 3.62e-7;     % Lb-Sec/Ft^2
 P0 = 2116.2;       % Lb/Ft^2
 Rho0 = 0.002377;   % Slug/Ft^3
 aLR = -0.00349;     % R/Ft
+
+r = 6.356766E6; %radius of E in ft
+
+alpha_temp  =  0.00357; %temperature lapse rate R/ft
+rho1 = 0.002377; % slug/ft^3 density SL standard day
+gamma = 1.4;
+S = 198.7; %sutherland constant
+r_planet_radius = 6.356766*10^6; 
+h1_Assuming_Troposphere = 0; %Base atmosphere 
+
 % Add a call to another m file to initialize all LONG-VERT data here:
 
 
