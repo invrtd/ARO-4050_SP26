605c6785681adddede96ec458c101abdffb20a6f
invrtd <invrtd04.5@gmail.com>
2026-03-04 10:37:38 -0800

    Updated: Thrust

---
+++ init/Vert_thrust_vars.m
@@ -0,0 +1,13 @@
+%% Engine positions for moment calculations
+x_T_in = 27.1883; % [ft]
+y_T_in = 38.3914; % [ft]
+z_T_in = 11.5761; % [ft]
+x_T_out = 1.1857; % [ft]
+y_T_out = 68.5432; % [ft]
+z_T_out = 8.4480; % [ft]
+psi_T = 0; % [rad] Assume zero for now
+phi_T = 0; % [rad] Assume zero for now 
+
+%% Engine Thrust
+T_Max0 = 56700; % pounds force
+NumberOfEngines = 4;
