09bf75c6ceda5b2139b839d158e2d85eaea4b1c0
invrtd <invrtd04.5@gmail.com>
2026-03-22 15:47:53 -0700

    Added: FastRestart Fixed: Reading moments

--- Estimation/Sim_Trim.m
+++ Estimation/Sim_Trim.m
@@ -3,7 +3,9 @@
 StabTrimPos = -1.516;      % Horizontal Stabilizer Setting (+ = Nose Up Trim, deg or rad depending on model)
 EngineThrustCMD = 0.793;   % N1 Fan Speed (1.0 = 100% Max, 0.0 = 0% Min
 %look in Sim_Init
-modelname = 'ARO40506DOF_2020a_2025'
+modelname = 'ARO40506DOF_2020a_2025';
+tolerance =1e-6;
+max_iters=5;
 
 % 
 for i = 1:max_iters
@@ -15,12 +17,12 @@ for i = 1:max_iters
     q0 = 0; % Ensure rotation starts at zero
 
     % 2. Run a short "Snapshot" Simulation
-    simOut = sim(modelname, 'StopTime', '0.01');
+    simOut = sim(modelname, 'StopTime', '0.001','FastRestart', "on");%makes runs faster now 
 
     % 3. Robust Data Extraction via getElement
-    %M_val  = simOut.logsout.getElement('Net Body Moments').Values.Data(1);
+    M_val  = simOut.logsout.getElement('Net Body Moments').Values.M.Data(1);%since it is a bus you need to say which val to read
     Ax_val = simOut.logsout.getElement('u dot (Ft/Sec^2)').Values.Data(1);
-    Az_val = simOut.logsout.getElement('v dot (Ft/Sec^2)').Values.Data(1);
+    Az_val = simOut.logsout.getElement('w dot (Ft/Sec^2)').Values.Data(1);
 
     % Log history for progressive tracking
    % history = [history; i, Ax_val, Az_val, alpha, StabTrimPos, EngineThrustCMD];
@@ -34,7 +36,7 @@ for i = 1:max_iters
     % 5. Simultaneous Updates (Gradient Descent logic)
     % Update Alpha (Balances Lift vs Weight)
     % If Az is negative (sinking), we need more Alpha.
-    alpha = alpha + (Az_val * 0.45); 
+    alpha = alpha + (Az_val * 0.2); 
 
     % Update Thrust (Balances Thrust vs Drag)
     % If Ax is negative (slowing down), we need more Thrust.
@@ -44,7 +46,9 @@ for i = 1:max_iters
     % M_ih_dim approx = Qbar * Sref * Cbar * CM_ih
 %     M_ih_dim = Qbar * Sref * Cbar * CM_ih/8;
 %     StabTrimPos = StabTrimPos - (M_val / M_ih_dim);
- end
+ fprintf('Final Trim State: Alpha: %.5f deg Stab: %.5f deg Thrust: %.2f%%\n', ...
+         alpha, StabTrimPos, EngineThrustCMD*100); 
+end
 % 
 % %% 6. Progressive Trim Results Visualization
 % % figure('Name', 'Trim Convergence History', 'Color', 'w');
@@ -52,5 +56,11 @@ for i = 1:max_iters
 % % subplot(3,1,2); plot(history(:,1), history(:,3), '-o'); title('Longitudinal Accel (Ax)'); grid on;
 % % subplot(3,1,3); plot(history(:,1), history(:,4), '-o'); title('Vertical Accel (Az)'); grid on;
 % 
-% fprintf('Final Trim State:\n Alpha: %.5f deg\n Stab: %.5f deg\n Thrust: %.2f%%\n', ...
-%         alpha, StabTrimPos, EngineThrustCMD*100);
+ 
+
+ sim(modelname,'StopTime', '0.001', 'FastRestart', "off");%set it back to normal mode
+
+
+ fprintf('Final Trim State:\n Alpha: %.5f deg\n Stab: %.5f deg\n Thrust: %.2f%%\n', ...
+         alpha, StabTrimPos, EngineThrustCMD*100);
+ 
