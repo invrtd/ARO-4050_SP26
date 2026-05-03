e8ddfd8f75a2992b3a77fc48a602990d808a9007
sammyv1 <samuelv1@cpp.edu>
2026-05-03 11:17:08 -0700

    Alt hold vertical outer loop  (#32)
    * Outer Loop Altitude hold Vert
    
    * Altitude Hold vert outer loop 
    Added vars onto estimate modes 
    % Alt Hold Gains (Outer Loop)
    selalt = 40000-50; % ft 40000+/-50
    Kp_alt = 0.00026; 
    Kd_alt = 0.01;
    Ki_alt = 0.000006;
    PitchIntegLim = 0.0873; % rad
    
    * Alt hold outer loop vert
    Added vars to estimate modes
    % Alt Hold Gains (Outer Loop)
    selalt = 40000-50; % ft 40000+/-50
    Kp_alt = 0.00026; 
    Kd_alt = 0.01;
    Ki_alt = 0.000006;
    PitchIntegLim = 0.0873; % rad
    
    * Did not infact cop block correctly, Now fixed
    
    * Sim break so have to disable Alt hold
    
    ---------
    
    Co-authored-by: samuelvelasquez <samuelvelasquez@Mac.lan>
    Co-authored-by: invrtd <invrtd04.5@gmail.com>

--- init/Sim_Init.m
+++ init/Sim_Init.m
@@ -69,8 +69,8 @@ Kp_pitch = 0.0;
 Kd_pitch = 0.0;
 Kp_roll  = 0.0;
 Kd_roll  = 0.0;
-Ktheta = 7;
-Kthetadot = 8;
+Ktheta = 6;
+Kthetadot = 10;
 % --- LOES Gains ---
 Kp_pitch_LOES = 0.5;
 % Sim_Trim
